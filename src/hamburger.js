561fc31a9075e8b9f76257afff6a251354fc63e5
zsoit <wsparcie.exe@gmail.com>
2020-11-25 22:26:15 +0100

    Initial Commmit

---
+++ src/hamburger.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.navigation');
+
+const handleClick = () => {
+hamburger.classList.toggle('hamburger--active');
+nav.classList.toggle('navigation--active');
+}
+
+hamburger.addEventListener('click', handleClick);
